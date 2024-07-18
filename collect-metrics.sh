d50067f245936e7d04a43d0ec53436c53ea1b63e
yihau <yihau.chen@icloud.com>
2024-07-18 23:38:54 +0800

    update script

--- collect-metrics.sh
+++ collect-metrics.sh
@@ -1,37 +1,47 @@
 #!/usr/bin/env bash
 
-tcpdump -i any -l '(portrange 8000-8020 or port 8899 or port 8900)' | while read -r line; do
-  direction=""
-  if [[ $(echo "$line" | awk '{print $3}') =~ ^labs-staked-node-ld7\. ]]; then
-      direction="OUT"
-  elif [[ $(echo "$line" | awk '{print $5}') =~ ^labs-staked-node-ld7\. ]]; then
-      direction="IN"
-  fi
-  if [[ -z $direction ]]; then
-    echo >&2 "couldn't parse 'direction' from line: $line"
-    continue
-  fi
-  direction=${direction^^}
+MY_IP=
 
-  protocol=$(echo "$line" | awk '{print $6}' | tr -d ',')
+tcpdump -n -t -i bond0 -l \
+  "((src host $MY_IP and src portrange 8000-10000) or \
+  (dst host $MY_IP and dst portrange 8000-10000)) and (tcp or udp)" | while read -r line; do
+
+  maybe_protocol=$(echo "$line" | awk '{print $5}')
+  protocol=""
+  if [[ "$maybe_protocol" =~ UDP ]]; then
+    protocol="UDP"
+  elif [[ "$maybe_protocol" =~ Flags ]]; then
+    protocol="TCP"
+  fi
   if [[ -z $protocol ]]; then
-    echo >&2 "couldn't parse 'protocol' from line: $line"
+    echo >&2 "unexpected package: $line"
     continue
   fi
 
-  length=$(echo "$line" | awk '{print $NF}')
-  if [[ -z $length ]]; then
-    echo >&2 "couldn't parse 'length' from line: $line"
-    continue
+  length=0
+  if [[ "$line" =~ length\ ([0-9]+) ]]; then
+    length="${BASH_REMATCH[1]}"
   fi
 
-  if [[ $direction == "IN" ]]; then
-    port=$(echo "$line" | awk '{print $5}' | sed -E 's/.*\.([0-9]+):.*/\1/')
-  elif [[ $direction == "OUT" ]]; then
-    port=$(echo "$line" | awk '{print $3}' | sed -E 's/.*\.([0-9]+).*/\1/')
+  src=$(echo "$line" | awk '{print $2}')
+  dst=$(echo "$line" | awk '{print $4}')
+  direction=""
+  port=""
+  if [[ $dst =~ $MY_IP ]]; then
+    direction="IN"
+    port=$(echo "$dst" | awk -F'[.]' '{print $NF}' | tr -d ':')
+  elif [[ $src =~ $MY_IP ]]; then
+    direction="OUT"
+    port=$(echo "$src" | awk -F'[.]' '{print $NF}' | tr -d ':')
   fi
+
   if [[ -z $port ]]; then
-    echo >&2 "couldn't parse 'port' from line: $line"
+    echo >&2 "couldn't parse 'port' from the line: $line"
+    continue
+  fi
+
+  if [[ -z $direction ]]; then
+    echo >&2 "couldn't parse 'direction' from the line: $line"
     continue
   fi
 
